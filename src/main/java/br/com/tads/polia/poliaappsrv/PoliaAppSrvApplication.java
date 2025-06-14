a4ef8708bcafaf2842990d38079e1a90ec9b3038
Rafael Cecyn <rafael.mendes112@hotmail.com>
2025-06-14 16:53:18 -0300

    feat initial commit with hexagonal architecture

---
+++ src/main/java/br/com/tads/polia/poliaappsrv/PoliaAppSrvApplication.java
@@ -0,0 +1,13 @@
+package br.com.tads.polia.poliaappsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliaAppSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliaAppSrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tads/polia/poliaappsrv/PoliaAppSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tads.polia.poliaappsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliaAppSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
